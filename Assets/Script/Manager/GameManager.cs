82812290d6dd85dd9ca9ee64fe8d923c43d84b5a
kimhyeonung <154565514+kimhyeonung@users.noreply.github.com>
2026-01-28 21:01:40 +0900

    a

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    #region Warping Stages
     /// <summary>
     /// 0. 뒤틀림 1단계
     /// 1. 뒤틀림 중간 단계
     /// 2. 뒤틀림 2단계
     /// </summary>
-    public bool[] isWarpingStages; 
-
+    public bool[] isWarpingStages;
+    #endregion
     void Start()
     {
         
