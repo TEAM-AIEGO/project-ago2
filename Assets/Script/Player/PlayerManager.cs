55cd754e58b54f869d8e277a2307b4791a786479
kimhyeonung <154565514+kimhyeonung@users.noreply.github.com>
2026-01-28 20:52:36 +0900

    Warping Stages

--- Assets/Script/Player/PlayerManager.cs
+++ Assets/Script/Player/PlayerManager.cs
@@ -10,5 +10,29 @@ public class PlayerManager : Unit
     void Update()
     {
         
+        
+    }
+
+    void OnWarpingStagesChange()
+    {
+        for (int i = 0; i < 3; i++)
+            GameManager.Instance.isWarpingStages[i] = false;
+
+        if (Health >= 0 && Health <= 100)
+            GameManager.Instance.isWarpingStages[0] = true;
+        else if (Health > 100 && Health < 200)
+            GameManager.Instance.isWarpingStages[1] = true;
+        else
+            GameManager.Instance.isWarpingStages[2] = true;
+    }
+
+    void OnGetDamage(int damage)
+    {
+        Health -= damage;
+        OnWarpingStagesChange();
+        if (Health <= 0)
+        {
+            // dead
+        }
     }
 }
