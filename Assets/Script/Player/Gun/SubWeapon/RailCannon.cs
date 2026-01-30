968b998b762ee9ff9a858cb66b589d0a4ba843eb
Heonus <xhdxhdrod22@gmail.com>
2026-01-30 18:25:07 +0900

    oiiai

--- Assets/Script/Player/Gun/SubWeapon/RailCannon.cs
+++ Assets/Script/Player/Gun/SubWeapon/RailCannon.cs
@@ -114,7 +114,7 @@ public class RailCannon : SubWeapon
 
         objectPool.SpawnProjectile(railCannonProjectilePrefab, projectileLunchPoint.position, Quaternion.LookRotation(direction));
         emitter.PlayFollow("Rail_Cannon_Fire", playerTransform);
-        cameraShake?.AddRecoil(new Vector2(Random.Range(-0.2f, 0.2f), 1.5f));
+        cameraShake?.AddRecoil(new Vector2(Random.Range(-2f, 2f), 10f));
         isInAftereffect = true;
     }
 }
