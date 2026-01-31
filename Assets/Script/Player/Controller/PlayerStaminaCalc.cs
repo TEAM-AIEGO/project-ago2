c613644df625f8147848b93e1c0d1817d73a3bd6
kimhyeonung <154565514+kimhyeonung@users.noreply.github.com>
2026-01-31 16:46:13 +0900

    Stamina Bar UI

--- Assets/Script/Player/Controller/PlayerStaminaCalc.cs
+++ Assets/Script/Player/Controller/PlayerStaminaCalc.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 
-public class PlayerStaminaCalc : MonoBehaviour
+public class PlayerStaminaCalc : MonoBehaviour, IStat
 {
     private PlayerController playerController;
+
+    #region IStat
+    public float CurrentValue => CurrentStamina;
+    public float MaxValue => MaxStamina;
+    public event System.Action<float, float> OnValueChanged;
+    #endregion
 
     [Header("Stamina Settings")]
     [SerializeField] private float CurrentStamina;
     [SerializeField] private float MaxStamina;
     [SerializeField] private float StaminaRegenSpeed;
     [SerializeField] private float DeshStaminaCost;
+
+    private void Awake()
+    {
+        OnValueChanged?.Invoke(CurrentStamina, MaxStamina);
+    }
 
     public void Initialized(PlayerController playerController)
     {
@@ -22,7 +33,7 @@ public class PlayerStaminaCalc : MonoBehaviour
             //Debug.Log("Regenerating Stamina");
             CurrentStamina += StaminaRegenSpeed * Time.deltaTime;
             CurrentStamina = Mathf.Min(CurrentStamina, MaxStamina);
-
+            OnValueChanged?.Invoke(CurrentStamina, MaxStamina);
             if (playerController.CannotSprint)
                 if (CurrentStamina >= MaxStamina / 5.5)
                 {
@@ -38,6 +49,7 @@ public class PlayerStaminaCalc : MonoBehaviour
         CurrentStamina -= DeshStaminaCost;
         
         CurrentStamina = Mathf.Max(CurrentStamina, 0);
+        OnValueChanged?.Invoke(CurrentStamina, MaxStamina);
         //Debug.Log("Current Stamina: " + CurrentStamina);
         playerController.DeshAble(false);
         /* if (CurrentStamina == 0)
