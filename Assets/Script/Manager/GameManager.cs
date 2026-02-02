582d72a0d31e54ed7b9ebbaded864d02d54e0443
Heonus <xhdxhdrod22@gmail.com>
2026-02-02 22:04:28 +0900

    Stage Manager

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -4,33 +4,8 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
 
-[Serializable]
-public enum StageState
-{
-    Loading,
-    Fighting,
-    Ended
-}
-
-[Serializable]
-public struct EnemySpawnData
-{
-    public GameObject EnemyPrefab;
-    public Vector3 EnemySpawnPosition;
-}
-
-[Serializable]
-public class Stage
-{
-    public StageState StageState;
-    public GameObject StageTarget;
-    public List<EnemySpawnData> Enemies;
-}
-
 public class GameManager : MonoBehaviour
 {
-
-    public List<Stage> Stages;
     public bool IsGameOver { get; private set; }
 
     #region Warping Stages
@@ -46,81 +21,13 @@ public class GameManager : MonoBehaviour
     //
     //
 
-    [SerializeField] private ObjectPool objectPool;
-    private int currentStageIndex = 0;
-    private int StageEnemyLeft = 0;
+    [SerializeField] private StageManager stageManager;
 
 
-    void Start()
+    private void Awake()
     {
-        StartStage(0);
-    }
+        stageManager.Initialize();
 
-    void StartStage(int stageIndex)
-    {
-        if (Stages[stageIndex] == default(Stage)) return;
-        var currentStage = Stages[stageIndex];
-        currentStage.StageState = StageState.Loading;
-        currentStageIndex = stageIndex;
-        StageEnemyLeft = currentStage.Enemies.Count;
-        for (int i = 0; i < currentStage.Enemies.Count; i++)
-        {
-            var currentEnemySpawnData = currentStage.Enemies[i];
-            //GameObject currentEnemy = Instantiate(currentEnemySpawnData.EnemyPrefab, currentEnemySpawnData.EnemySpawnPosition, quaternion.identity);
-            //if (currentEnemy.TryGetComponent(out EnemyBase enemyBase))
-            //{
-            //    enemyBase.Initialize(enemyBase);
-            //}
-            //currentEnemy.GetComponent<Unit>().Died.AddListener(OnStageEnemyKilled);
-
-            EnemyBase currentEnemy = objectPool.SpawnEnemy(currentEnemySpawnData.EnemyPrefab.GetComponent<EnemyBase>(), currentEnemySpawnData.EnemySpawnPosition);
-            RegisterEnemy(currentEnemy, false);
-        }
-        currentStage.StageState = StageState.Fighting;
-    }
-
-
-    private void RegisterEnemy(EnemyBase enemy, bool countForStage)
-    {
-        if (enemy == null)
-        {
-            return;
-        }
-
-        enemy.GetComponent<Unit>().Died.AddListener(OnStageEnemyKilled);
-
-        if (countForStage)
-        {
-            StageEnemyLeft++;
-        }
-
-        if (enemy is NegromancyEnemy negromancyEnemy)
-        {
-            negromancyEnemy.SpawnRequested += HandleNegromancySpawnRequest;
-        }
-    }
-
-    private void HandleNegromancySpawnRequest(EnemySpawnRequest request)
-    {
-        if (request.Prefab == null)
-        {
-            return;
-        }
-
-        EnemyBase spawnedEnemy = objectPool.SpawnEnemy(request.Prefab, request.Position);
-        RegisterEnemy(spawnedEnemy, true);
-    }
-
-
-    void OnStageEnemyKilled()
-    {
-        StageEnemyLeft--;
-        if (StageEnemyLeft <= 0)
-        {
-            Stages[currentStageIndex].StageState = StageState.Ended;
-            Stages[currentStageIndex].StageTarget.SetActive(false);
-            StartStage(++currentStageIndex);
-        }
     }
 
     void Update()
---
+++ Assets/Script/Manager/StageManager.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public enum StageState
+{
+    Loading,
+    Fighting,
+    Ended
+}
+
+[Serializable]
+public struct EnemySpawnData
+{
+    public GameObject EnemyPrefab;
+    public Vector3 EnemySpawnPosition;
+}
+
+[Serializable]
+public class Stage
+{
+    public StageState StageState;
+    public GameObject StageObject;
+    public GameObject WarpStageObject;
+    public GameObject StageTarget;
+    public List<EnemySpawnData> Enemies;
+}
+
+public class StageManager : MonoBehaviour, IWarpObserver
+{
+    public List<Stage> Stages;
+    public bool IsGameOver { get; private set; }
+
+    [SerializeField] private ObjectPool objectPool;
+    [SerializeField] private WarpSystemManager warpSystemManager;
+    private int currentStageIndex = 0;
+    private int StageEnemyLeft = 0;
+
+    public void Initialize()
+    {
+        currentStageIndex = 0;
+        StageEnemyLeft = 0;
+
+        warpSystemManager.RegisterWarpObserver(this);
+        StartStage(currentStageIndex);
+    }
+
+    private void OnDisable()
+    {
+        warpSystemManager.UnregisterWarpObserver(this);
+    }
+
+    public void StartStage(int stageIndex)
+    {
+        if (Stages[stageIndex] == default(Stage)) return;
+        var currentStage = Stages[stageIndex];
+        currentStage.StageState = StageState.Loading;
+        currentStageIndex = stageIndex;
+        StageEnemyLeft = currentStage.Enemies.Count;
+        for (int i = 0; i < currentStage.Enemies.Count; i++)
+        {
+            var currentEnemySpawnData = currentStage.Enemies[i];
+
+            EnemyBase currentEnemy = objectPool.SpawnEnemy(currentEnemySpawnData.EnemyPrefab.GetComponent<EnemyBase>(), currentEnemySpawnData.EnemySpawnPosition);
+            RegisterEnemy(currentEnemy, false);
+        }
+        currentStage.StageState = StageState.Fighting;
+    }
+
+    private void RegisterEnemy(EnemyBase enemy, bool countForStage)
+    {
+        if (enemy == null)
+        {
+            return;
+        }
+
+        enemy.GetComponent<Unit>().Died.AddListener(OnStageEnemyKilled);
+
+        if (countForStage)
+        {
+            StageEnemyLeft++;
+        }
+
+        if (enemy is NegromancyEnemy negromancyEnemy)
+        {
+            negromancyEnemy.SpawnRequested += HandleNegromancySpawnRequest;
+        }
+    }
+
+    private void HandleNegromancySpawnRequest(EnemySpawnRequest request)
+    {
+        if (request.Prefab == null)
+        {
+            return;
+        }
+
+        EnemyBase spawnedEnemy = objectPool.SpawnEnemy(request.Prefab, request.Position);
+        RegisterEnemy(spawnedEnemy, true);
+    }
+
+    void OnStageEnemyKilled()
+    {
+        StageEnemyLeft--;
+        if (StageEnemyLeft <= 0)
+        {
+            Stages[currentStageIndex].StageState = StageState.Ended;
+            Stages[currentStageIndex].StageTarget.SetActive(false);
+            StartStage(++currentStageIndex);
+        }
+    }
+
+    public void OnWarpStageChanged(int newStage)
+    {
+        StageChange(newStage);
+    }
+
+    private void StageChange(int newStageIndex)
+    {
+        //
+    }
+}
