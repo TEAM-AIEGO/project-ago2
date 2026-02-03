4c80f83a48c57c1c976ebc6da7d32f90b8435b6e
mitsu-shirobo <mitsupowan.daa@protonmail.com>
2026-02-03 21:09:19 +0900

    scene cutted (haha, got it?)

--- Assets/Script/TestScripts/Playthething.cs
+++ Assets/Script/TestScripts/Playthething.cs
@@ -4,10 +4,16 @@ using UnityEngine.Playables;
 [RequireComponent(typeof(PlayableDirector))]
 public class Playthething : MonoBehaviour
 {
+    [SerializeField] Camera PlayCam;
     private PlayableDirector pd;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlaythethingPlz()
     {
+        if (PlayCam)
+        {
+            Camera.main.enabled = false;
+            PlayCam.enabled = true;
+        }
         Time.timeScale = 0.5f;
         pd = GetComponent<PlayableDirector>();
         pd.Play();
