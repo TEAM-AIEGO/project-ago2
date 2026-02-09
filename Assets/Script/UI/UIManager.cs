3a82d9219aba2ee5618649d335f438635b2a5d1c
kimhyeonung <154565514+kimhyeonung@users.noreply.github.com>
2026-02-09 16:08:20 +0900

    SubTitle

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] private HitMarker hitMarker;
     [SerializeField] private Image DoorAlarm;
     [SerializeField] private Image SubWeaponFrame;
+    [SerializeField] private GameObject subtitleRoot;
+    [SerializeField] private TMP_Text subtitleText;
 
     private void Start()
     {
@@ -34,5 +37,25 @@ public class UIManager : MonoBehaviour
             SubWeaponFrame.gameObject.SetActive(false);
         else
             SubWeaponFrame.gameObject.SetActive(true);
+    }
+
+    public void SetSubtitleActive(bool isActive)
+    {
+        if (subtitleRoot == null)
+        {
+            return;
+        }
+
+        subtitleRoot.SetActive(isActive);
+    }
+
+    public void SetSubtitleText(string text)
+    {
+        if (subtitleText == null)
+        {
+            return;
+        }
+
+        subtitleText.text = $"{text}";
     }
 }
