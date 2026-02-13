82dcf5e472c3b455f87be424f2f9842842a2745e
kimhyeonung <154565514+kimhyeonung@users.noreply.github.com>
2026-02-14 00:36:22 +0900

    Title Scene

--- Assets/Script/Manager/TitleManager.cs
+++ Assets/Script/Manager/TitleManager.cs
@@ -2,9 +2,28 @@ using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] private GameObject guideUiBG;
+
     public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("OutdoorsScene");
+    }
+
+    public void Guide()
+    {
+        if (guideUiBG.activeInHierarchy)
+        {
+            guideUiBG.SetActive(false);
+        }
+        else
+        {
+            guideUiBG.SetActive(true);
+        }
+    }
+
+    public void GuideBgClose()
+    {
+        guideUiBG.SetActive(false);
     }
 
     public void ExitGame()
