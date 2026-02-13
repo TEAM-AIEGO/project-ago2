61f0c62afad92a88e814f032ca0a249faae92f28
Heonus <xhdxhdrod22@gmail.com>
2026-02-14 00:44:09 +0900

    Ranged Bot

--- Assets/Script/Enemy/RangedEnemy.cs
+++ Assets/Script/Enemy/RangedEnemy.cs
@@ -13,15 +13,13 @@ public class RangedEnemy : EnemyBase
 {
     [HideInInspector] public event Action<Projectile, Vector3, Quaternion, float, float> OnShootProjectile;
 
-    [SerializeField] private MeshRenderer rUp;
-    [SerializeField] private MeshRenderer rDown;
     [SerializeField] private Transform ShootPoint;
     [SerializeField] private float projectileSpeed = 40f;
     [SerializeField] private float maxLeadTime = 2f;
     [SerializeField] private HitFlash hitFlash;
     [SerializeField] private Projectile projectilePrefab;
-    [SerializeField] private GameObject FirstBody;
-    [SerializeField] private GameObject SecondBody;
+    [SerializeField] private GameObject head;
+    [SerializeField] private GameObject body;
 
     public override void Initialize(EnemyBase origin, int warpStage)
     {
@@ -63,6 +61,21 @@ public class RangedEnemy : EnemyBase
         }
     }
 
+    protected override void LookTarget()
+    {
+        base.LookTarget();
+        //Vector3 hDirection = (player.transform.position - head.transform.position).normalized;
+        //Quaternion hLookRotation = Quaternion.LookRotation(new Vector3(hDirection.x, hDirection.y, hDirection.z));
+        //head.transform.rotation = Quaternion.Slerp(head.transform.rotation, hLookRotation, Time.deltaTime * turnSpeed);
+
+        //if (muKatteKuruNoKaStrategy is MuKatteKuruNoKaStrategy)
+        //{
+        //    Vector3 direction = (player.transform.position - body.transform.position).normalized;
+        //    Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        //    body.transform.rotation = Quaternion.Slerp(head.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+        //}
+    }
+
     protected override void Attacking()
     {
         emitter.PlayFollow(AudioIds.RobotTurretRobotShot, transform, false, 0.4f, 0.7f);
@@ -73,28 +86,6 @@ public class RangedEnemy : EnemyBase
     public override void OnWarpStageChanged(int newStage)
     {
         base.OnWarpStageChanged(newStage);
-
-        if (textures.Length == 0) return;
-        switch (newStage)
-        {
-            case 0:
-                muKatteKuruNoKaStrategy = new DontMuKatteKuruNoKaStrategy();
-                SecondBody.SetActive(false);
-                FirstBody.SetActive(true);
-                rUp.material = textures[0];
-                rDown.material = textures[0];
-                break;
-            case 1:
-                muKatteKuruNoKaStrategy = new MuKatteKuruNoKaStrategy();
-                SecondBody.SetActive(true);
-                FirstBody.SetActive(false);
-                rUp.material = textures[1];
-                rDown.material = textures[1];
-                break;
-            default:
-                Debug.LogError("Invalid warp stage");
-                break;
-        }
     }
 
     public override void TakeDamage(float damage)
