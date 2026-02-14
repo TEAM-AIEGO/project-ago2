e6ee0e3ffc796f84021658ef6391f07086860788
mitsu-shirobo <mitsupowan.daa@protonmail.com>
2026-02-14 09:55:24 +0900

    fianlaiekjhf 3

--- Assets/Script/Others/FinalStatText.cs
+++ Assets/Script/Others/FinalStatText.cs
@@ -8,7 +8,7 @@ public class FinalStatText : MonoBehaviour
     void Start()
     {
         if (!text) return;
-        text.text = $"걸린 시간: {StaticValueManager.GameTime.ToString("0.##")}분   찾은 고양이: {StaticValueManager.CatsFound}/8"; 
+        text.text = $"걸린 시간: {StaticValueManager.GameTime.ToString("0.##")}초   찾은 고양이: {StaticValueManager.CatsFound}/8"; 
     }
 
     // Update is called once per frame
